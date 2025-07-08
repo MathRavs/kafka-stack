c36e9346a33337be32adb2ae6904412dca5230b0
MoonCypher <mathiasravelojaona@gmail.com>
2025-07-08 11:07:26 +0200

    fix(lambda): fix lambda documentation entry points

--- lambda/generate-presigned-url/fastapi_app/app/main.py
+++ lambda/generate-presigned-url/fastapi_app/app/main.py
@@ -1,6 +1,12 @@
+import os
 from fastapi import FastAPI
 
 from fastapi_app.app.documents.routes.documents_route import documents_router
 
-app = FastAPI()
+app = FastAPI(
+    docs_url="/docs",
+    redoc_url="/redoc",
+    openapi_url="/openapi.json",
+    root_path=f"/{os.getenv("STAGE_NAME", "Dev")}"
+)
 app.include_router(documents_router, prefix="/documents")
